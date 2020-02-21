9d2408b80a2f42e55537ec109bd7740d2798f6cc
viewdegree <viewdegree@163.com>
2020-02-22 07:58:29 +0800

    build:引入element的container布局

---
+++ element-admin/vue.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-22 07:47:07
+ * @LastEditTime: 2020-02-22 07:47:21
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \element-admin\vue.config.js
+ */
+module.exports = {
+    devServer:{
+        //配置打开端口
+        port: 3333,
+        //配置是否自动打开
+        open: true
+    }
+}
