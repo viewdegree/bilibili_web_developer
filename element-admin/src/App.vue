ea34ba737696364a59d3f8d17f51e014e3b6bb0b
viewdegree <viewdegree@163.com>
2020-02-22 08:29:56 +0800

    Build:配置好views文件夹下显示路由的页面

--- element-admin/src/App.vue
+++ element-admin/src/App.vue
@@ -25,14 +25,8 @@
       </el-header>
       
       <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140">
-          </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120">
-          </el-table-column>
-          <el-table-column prop="address" label="地址">
-          </el-table-column>
-        </el-table>
+        <!-- 用来对应每一个页面的组件内容 -->
+        <router-view></router-view>
       </el-main>
     </el-container>
   </el-container>
@@ -55,15 +49,6 @@
 
 <script>
   export default {
-    data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
-      return {
-        tableData: Array(20).fill(item)
-      }
-    }
+    
   };
 </script>
